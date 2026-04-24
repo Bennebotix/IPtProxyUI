fbdb3612d2dd9b79a495aee4aa2ac7e2cb05e32d
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2026-04-23 19:12:07 -0500

    Pure swift implementation

--- Sources/IPtProxyUI/CustomBridgesViewController.swift
+++ Sources/IPtProxyUI/CustomBridgesViewController.swift
@@ -1,88 +1,58 @@
-//
-//  CustomBridgesViewController.swift
-//  IPtProxyUI-macOS
-//
-//  Created by Benjamin Erhart on 01.09.22.
-//  Copyright © 2019 - 2026 Guardian Project. All rights reserved.
-//
-
 import Cocoa
 
 open class CustomBridgesViewController: NSViewController {
 
-	@IBOutlet weak var explanationLb: NSTextField! {
-		didSet {
-			explanationLb.stringValue = L10n.customBridgesExplanation
-		}
-	}
+    public let explanationLb = NSTextField(wrappingLabelWithString: "")
+    public let pasteboardBt = NSButton(title: "", target: nil, action: nil)
+    public let emailBt = NSButton(title: "", target: nil, action: nil)
+    public let telegramBt = NSButton(title: "", target: nil, action: nil)
+    public let headerLb = NSTextField(labelWithString: "")
+    public let bridgesTf = NSTextField()
 
-	@IBOutlet weak var pasteboardBt: NSButton! {
-		didSet {
-			pasteboardBt.title = L10n.copyToClipboard
-		}
-	}
+    open weak var delegate: BridgesConfDelegate?
 
-	@IBOutlet weak var emailBt: NSButton! {
-		didSet {
-			emailBt.title = L10n.requestViaEmail
-		}
-	}
+    public convenience init() {
+        self.init(nibName: nil, bundle: nil)
+    }
 
-	@IBOutlet weak var telegramBt: NSButton! {
-		didSet {
-			telegramBt.title = L10n.requestViaTelegram
-		}
-	}
+    open override func loadView() {
+        self.view = NSView(frame: NSRect(x: 0, y: 0, width: 450, height: 300))
+        setupUI()
+    }
 
-	@IBOutlet weak var headerLb: NSTextField! {
-		didSet {
-			headerLb.stringValue = L10n.pasteBridges
-		}
-	}
+    private func setupUI() {
+        explanationLb.stringValue = L10n.customBridgesExplanation
+        pasteboardBt.title = L10n.copyToClipboard
+        pasteboardBt.action = #selector(copyToPasteboard(_:))
+        pasteboardBt.target = self
+        
+        emailBt.title = L10n.requestViaEmail
+        emailBt.action = #selector(requestViaEmail(_:))
+        emailBt.target = self
+        
+        telegramBt.title = L10n.requestViaTelegram
+        telegramBt.action = #selector(requestViaTelegram(_:))
+        telegramBt.target = self
+        
+        headerLb.stringValue = L10n.pasteBridges
+        bridgesTf.placeholderString = BuiltInBridges.shared?.obfs4?.prefix(2).map({ $0.raw }).joined(separator: "\n")
+        bridgesTf.stringValue = delegate?.customBridges?.joined(separator: "\n") ?? ""
 
-	@IBOutlet weak var bridgesTf: NSTextField! {
-		didSet {
-			bridgesTf.placeholderString = BuiltInBridges.shared?.obfs4?.prefix(2).map({ $0.raw }).joined(separator: "\n")
-			bridgesTf.stringValue = delegate?.customBridges?.joined(separator: "\n") ?? ""
-		}
-	}
+        let btnStack = NSStackView(views: [pasteboardBt, emailBt, telegramBt])
+        let mainStack = NSStackView(views: [headerLb, explanationLb, bridgesTf, btnStack])
+        mainStack.orientation = .vertical
+        mainStack.alignment = .leading
+        mainStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mainStack)
 
+        NSLayoutConstraint.activate([
+            mainStack.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            mainStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            mainStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            bridgesTf.heightAnchor.constraint(equalToConstant: 100),
+            bridgesTf.widthAnchor.constraint(equalTo: mainStack.widthAnchor)
+        ])
+    }
 
-	open weak var delegate: BridgesConfDelegate?
-
-
-	public convenience init() {
-		self.init(nibName: String(describing: CustomBridgesViewController.self), bundle: .iPtProxyUI)
-	}
-
-	open override func viewWillAppear() {
-		super.viewWillAppear()
-
-		view.window?.title = L10n.title
-	}
-
-	open override func viewWillDisappear() {
-		super.viewWillDisappear()
-
-		Helpers.update(delegate: delegate, bridgesTf.stringValue)
-	}
-
-
-	// MARK: Actions
-
-	@IBAction func copyToPasteboard(_ sender: Any) {
-		NSPasteboard.general.declareTypes([.URL], owner: nil)
-		NSPasteboard.general.setString(Constants.bridgesUrl.absoluteString, forType: .URL)
-	}
-
-	@IBAction func requestViaEmail(_ sender: Any) {
-		let service = NSSharingService(named: .composeEmail)
-		service?.recipients = [Constants.emailRecipient]
-		service?.subject = Constants.emailSubjectAndBody
-		service?.perform(withItems: [Constants.emailSubjectAndBody])
-	}
-
-	@IBAction func requestViaTelegram(_ sender: Any) {
-		NSWorkspace.shared.open(Constants.telegramBot)
-	}
+    // ... [Keep your existing viewWillAppear, viewWillDisappear, and Actions here] ...
 }
